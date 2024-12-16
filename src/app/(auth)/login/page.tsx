ce6bf7f14270682e8c9d0edbdd492e7c19849f91
sen2y <pse6704@sookmyung.ac.kr>
2024-12-17 01:57:24 +0900

    ✨ 카카오 로그인/ 비로그인 진입 페이지 구현한다 - 토큰 접근 로직 구현 필요 ( 로그인 후 /login 접근불가 )

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
+import KakaoLogo from 'public/kakao.svg';
 import Logo from 'public/logo/logo.svg';
 
 const kakaoLoginHandler = async () => {
@@ -9,12 +12,28 @@ const kakaoLoginHandler = async () => {
 };
 
 const LoginPage = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="mt-20 flex h-full flex-col items-center justify-center">
       <Logo />
-      <button type="button" onClick={kakaoLoginHandler}>
-        kakao
+      <h1 className="mb-44 text-2xl font-semibold">퀴즈로 준비하는 테크 인터뷰</h1>
+
+      <button
+        type="button"
+        onClick={kakaoLoginHandler}
+        className="mb-4 flex h-14 w-full items-center justify-center gap-1.5 rounded-lg bg-yellow-300 text-[14.5px] font-medium"
+      >
+        <KakaoLogo width={20} height={20} />
+
+        <div>카카오톡으로 시작하기</div>
+      </button>
+      <button
+        type="button"
+        onClick={() => router.push('/')}
+        className="flex h-14 w-full items-center justify-center gap-1.5 rounded-lg bg-gray-100 text-[14.5px] font-medium"
+      >
+        <div>비로그인으로 시작하기</div>
       </button>
     </div>
   );
